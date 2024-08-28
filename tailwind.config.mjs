665203e53b2c8d12f8d78b61fa726a833b08806e
Vance Tan <vxncetxn@gmail.com>
2024-08-29 01:56:00 +0800

    Added tailwindcss-capsize; Added sans font and spacing tokens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["PP Neue Montreal", ...defaultTheme.fontFamily.sans],
+      // serif: ["PP Pangaia", ...defaultTheme.fontFamily.serif],
+    },
+    fontMetrics: {
+      sans: {
+        capHeight: 715,
+        ascent: 958,
+        descent: -242,
+        lineGap: 0,
+        unitsPerEm: 1000,
+      },
+    },
+    spacing: {
+      0: "0",
+      2: "0.125rem",
+      4: "0.25rem",
+      8: "0.5rem",
+      12: "0.75rem",
+      16: "1rem",
+      20: "1.25rem",
+      24: "1.5rem",
+      32: "2rem",
+      40: "2.5rem",
+      48: "3rem",
+      64: "4rem",
+      80: "5rem",
+      96: "6rem",
+      120: "7.5rem",
+      160: "10rem",
+      200: "12.5rem",
+      240: "15rem",
+      280: "17.5rem",
+      320: "20rem",
+      360: "22.5rem",
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-capsize")],
 };
